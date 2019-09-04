e7f7d2f6d07ef7a00674137f6746baf474d4d696
Ben Awad <benawad97@gmail.com>
2019-09-04 12:29:39 -0500

    initial commit

---
+++ src/ReducerExample.tsx
@@ -0,0 +1,45 @@
+import React, { useReducer } from "react";
+
+interface Props {}
+
+type Action =
+  | { type: "add"; text: string }
+  | {
+      type: "remove";
+      idx: number;
+    };
+
+interface Todo {
+  text: string;
+  complete: boolean;
+}
+
+type State = Todo[];
+
+const TodoReducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "add":
+      return [...state, { text: action.text, complete: false }];
+    case "remove":
+      return state.filter((_, i) => action.idx !== i);
+    default:
+      return state;
+  }
+};
+
+export const ReducerExample: React.FC<Props> = () => {
+  const [todos, dispatch] = useReducer(TodoReducer, []);
+
+  return (
+    <div>
+      {JSON.stringify(todos)}
+      <button
+        onClick={() => {
+          dispatch({ type: "add", text: "..." });
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
+};
---
+++ src/TextField.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const TextField = () => {
+  return (
+    <div>
+      <input />
+    </div>
+  );
+};
